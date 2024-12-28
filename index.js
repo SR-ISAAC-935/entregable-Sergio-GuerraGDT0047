d43f5cf2874c542189d757eac01fe68407049f05
SergioEmenergencias <Isaac7Guerra@gmail.com>
2024-12-28 15:20:25 -0600

    tercer commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express= require('express')
 const port=3000;
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const HomeRoutes= require('./Routes/HomeRoutes')
 const authRoutes= require('./Routes/AuthRoutes')
 const adminRoutes = require('./Routes/adminRoutes')
 const app = express();
+// CORS configuration
+app.use(cors({
+    credentials: true,
+    origin: process.env.PATHRENDER || '*',
+    methods: ['GET', 'POST']
+}));
+
 app.use(express.json())
 app.use(cookieParser())
 app.use('/auth',authRoutes )
